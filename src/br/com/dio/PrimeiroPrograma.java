e2703a956914bdae22a4c312256b48c5351dd654
Marina <marinascalbuquerque@gmail.com>
2022-03-19 12:17:28 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package br.com.dio;
+
+import br.com.dio.model.Cachorro;
+
+public class PrimeiroPrograma {
+    public static void main( String[] args){
+
+        Cachorro cachorro = new Cachorro();
+
+        System.out.println(cachorro);
+
+            /*        int a = 5;
+        int b = 3;
+        System.out.println("Marina Linda! ");*/
+
+    }
+}
